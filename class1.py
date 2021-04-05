d81c0385430d2ed7986485b72a2526dfa0d6df0b
ilyes-kerkeni <kerkenielyes@gmail.com>
2021-04-05 14:40:53 +0100

    conversion d'image en niveau de gris

---
+++ class1.py
@@ -0,0 +1,30 @@
+import numpy as np 
+import matplotlib.image as im
+import matplotlib.pyplot as plt 
+
+
+def lire_image(nom_fichier):
+
+    image=im.imread(nom_fichier)
+    return image
+
+def afficher_image(I):
+    plt.imshow(I,'gray')
+    plt.show()
+
+def rgb2gray(I):
+    c=I.shape[0]
+    l=I.shape[1]
+    g=np.zeros((c,l))
+    for i in range(c):
+        for j in range(l):
+            g[i,j]=sum(I[i,j,:])/3
+    return g
+
+
+
+
+
+im=lire_image("python.jpeg")
+grey=rgb2gray(im)
+afficher_image(grey)
